6a918eef9c2c593c7a49813ce475cf47c41480de
HemjalCF <abu.hemjal@connectedfinland.fi>
2020-02-23 12:59:32 +0200

    testing git

--- LED BLINK/src/main.c
+++ LED BLINK/src/main.c
@@ -19,7 +19,6 @@ void LED_Init() {
   GPIO_InitStruct.Pin = LED_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
- // GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
 }
