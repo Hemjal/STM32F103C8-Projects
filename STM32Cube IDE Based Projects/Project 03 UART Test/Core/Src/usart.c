5edaabe4701b226ed36df9e82571f6efd59589f0
HemjalCF <abu.hemjal@connectedfinland.fi>
2020-05-03 22:51:51 +0300

    Rx functionality added and works successfully

--- STM32Cube IDE Based Projects/Project 03 UART Test/Core/Src/usart.c
+++ STM32Cube IDE Based Projects/Project 03 UART Test/Core/Src/usart.c
@@ -4,12 +4,12 @@
 
 void USART_transmit(UART_HandleTypeDef *huart, char *data )
 {
-	HAL_UART_Transmit(huart, (uint8_t *) data, strlen(data), 100);
+	HAL_UART_Transmit(huart, (uint8_t *) data, strlen(data), 1000);
 	char newline[2] = "\r\n";
 	HAL_UART_Transmit(huart, (uint8_t *) newline, 2, 10);
 }
 
 void USART_receive(UART_HandleTypeDef *huart,char *data, uint8_t size )
 {
-	HAL_UART_Receive(huart, (uint8_t *)data, size, 1000);
+	HAL_UART_Receive(huart, (uint8_t *)data, size, 5000);
 }
